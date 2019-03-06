e5ceebde01d5a335ab8db731f6a0b027448745f2
Lachlan Priest <lachlanpriest@gmail.com>
2019-03-06 11:59:28 +0800

    Add meta to enumerable collection

--- lib/asknicely/enumerable_resource_collection.rb
+++ lib/asknicely/enumerable_resource_collection.rb
@@ -1,4 +1,9 @@
 module Asknicely
   class EnumerableResourceCollection < Array
+    attr_accessor :meta
+    def initialize(collection, meta)
+      super(collection)
+      @meta = meta
+    end
   end
 end
